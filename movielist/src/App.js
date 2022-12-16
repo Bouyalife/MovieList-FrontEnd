24dd8c155d8b52b036bbbb0c478960fdd5953372
Raffi <Raffi@DESKTOP-86NFDVV>
2022-12-16 22:04:46 +0100

    Commit, Added text input to search for movie with title. Possible to add single movie

--- movielist/src/App.js
+++ movielist/src/App.js
@@ -6,14 +6,43 @@ import react,{useEffect,useState} from 'react';
 function App() {
 
   const [catFact, setCatFact] = useState("");
+  const [movieData,setMovieData] = useState([]);
+  const [list,setList] = useState([]);
   
   useEffect(()=>{
       axios.get("http://localhost:8080/getcatfact").then(function(response){
          setCatFact(JSON.stringify(response.data));
       })
-  },[]);
+  },[])
 
   
+  function searchMovieWithId(event){
+    if(event.key === 'Enter')
+    {
+      console.log("http://localhost:8080/getmovie?id=" + event.target.value);
+      axios.get("http://localhost:8080/getmovie?id=" + event.target.value).then(function(response){
+        console.log(response.data);
+        setMovieData(response.data.results);
+      })
+    }
+  }
+
+  useEffect(() =>{
+    console.log("hit2");
+    console.log("length: " + localStorage.length)
+    if(localStorage.length > 1){
+      console.log("Hit")
+      setList(localStorage.getItem("movie"))
+    }
+    
+  },[])
+  
+
+  const addMovieToList = (event,title)=> {
+    console.log(title);
+    setList(title);
+    localStorage.setItem('movie',title)
+  }
 
   return (
       <div id="app">
@@ -22,6 +51,9 @@ function App() {
         </div>
         <div id="catFact-header">CatFact</div>
         <div id="catFact">{catFact}</div>
+        <div>Your list: {list}</div>
+        <div><input type="text" onKeyDown={searchMovieWithId}></input></div>
+        <div>{movieData.map((movie,index)=>(<div id="searchedMovies">{"Title: " + movie.original_title + " Release year: " + movie.release_date + " Popularity: " + movie.popularity} <button onClick={event => addMovieToList(event,movie.original_title)}>Add to List</button></div>))}</div>
       </div>
   );
 }
